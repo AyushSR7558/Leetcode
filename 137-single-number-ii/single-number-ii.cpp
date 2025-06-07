a8fb7b27e71f0bcc8624edb40ccdfd1b8f86496c
AyushSR7558 <ayushraut7558@gmail.com>
2025-06-07 10:19:52 +0530

    Time: 1 ms (44.94%) | Memory: 13.5 MB (37.00%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < n; i += 3) {
-            if (nums[i] != nums[i - 1]) {
-                cout<<nums[i]<<" "<<nums[i-1];
-                return nums[i - 1];
+        int n =nums.size();
+        int ans =0;
+        for(int i=0;i<=31;i++){
+            int sum =0;
+            for(int j=0;j<n;j++){
+                if(nums[j] & (1<<i)) sum++;
+            }
+            if(sum%3 != 0){
+                ans =(ans | (1<<i));
             }
         }
-        return nums[n - 1];
+        return ans;
     }
 };
