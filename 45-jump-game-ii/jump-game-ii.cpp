47cbae9f8ddab1bce3c497fdeb1c1c39eac3d566
AyushSR7558 <ayushraut7558@gmail.com>
2025-06-08 20:06:15 +0530

    Time: 61 ms (20.96%) | Memory: 21.4 MB (25.76%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> dp;
+//     int helper(vector<int>& nums,int idx){
+//         if(idx==nums.size()-1){
+//             return 0;
+//         }
+//         if(idx>=nums.size()){
+//             return 10000;
+//         }
+//         if(dp[idx]!=-1) return dp[idx];
+//         int minSteps =10000;
+//         for(int i=1;i<=nums[idx];i++){
+//             minSteps =  min(1 + helper(nums,idx+i),minSteps) ;
+//         }
+//         return dp[idx] =minSteps;
+//     }
+//     int jump(vector<int>& nums) {
+//         dp.resize(nums.size(),-1);
+//         return helper(nums,0);
+
+
+
+//     }
+// };
 class Solution {
 public:
-    vector<int> dp;
-    int helper(vector<int>& nums,int idx){
-        if(idx==nums.size()-1){
-            return 0;
-        }
-        if(idx>=nums.size()){
-            return 10000;
-        }
-        if(dp[idx]!=-1) return dp[idx];
-        int minSteps =10000;
-        for(int i=1;i<=nums[idx];i++){
-            minSteps =  min(1 + helper(nums,idx+i),minSteps) ;
-        }
-        return dp[idx] =minSteps;
-    }
     int jump(vector<int>& nums) {
-        dp.resize(nums.size(),-1);
-        return helper(nums,0);
+        vector<int>  dp(nums.size(),0);
+        for(int indx = nums.size() - 2; indx >= 0; indx--){
+            int minSteps =10000;
+            for(int i=1;i<=nums[indx];i++){
+                if(indx+i>=nums.size()) continue;
+                minSteps =  min(1 + dp[indx + i],minSteps) ;
+            }
+            dp[indx] =minSteps;
+        }
+        return dp[0];
     }
 };
