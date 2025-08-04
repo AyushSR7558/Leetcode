a59e10faad765afb7485cea3c9e034b1bb1293de
AyushSR7558 <ayushraut7558@gmail.com>
2025-08-04 11:17:05 +0530

    Time: 39 ms (63.30%) | Memory: 79 MB (8.05%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        /*
-            Here we will require three pointer's 
-            * first : first type of fruit
-            * second : second type of fruit
-            * third : previson consecutive
-        */
         int n = fruits.size();
-        int fruit1 = -1, fruit2 = -1, prevCons = 0, count = 0, maxi = 0;
-        for(int i = 0; i < n; i++){
-            if(fruit1 == -1){
-                fruit1 = fruits[i];
+        map<int,int> basket;
+        int left = 0, right = 0, maxi = 0;
+        while(right < n){
+            basket[fruits[right]]++;
+            
+            while(basket.size() > 2){
+                basket[fruits[left]]--;
+
+                if(basket[fruits[left]] == 0){
+                    basket.erase(fruits[left]);
+                }
+
+                left++;
             }
-            if(fruit1 != fruits[i] && fruit2 == -1 ){
-                fruit2 = fruits[i];
-            }
-            if(fruits[i] == fruit1 || fruits[i] == fruit2){
-                count++;
-            }else{
-                fruit1 = fruits[prevCons];
-                fruit2 = fruits[i];
-                count = i - prevCons + 1;
-            }
-            maxi = max(count,maxi);
-            if(i > 0 && fruits[i - 1] != fruits[i]){
-                prevCons = i;
-            }
+            maxi = max(maxi, right- left + 1);
+            right++;
         }
         return maxi;
     }
