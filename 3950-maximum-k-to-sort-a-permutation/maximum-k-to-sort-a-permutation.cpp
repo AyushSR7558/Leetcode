56f6f1b62e5a01d8272f8d8773793edb954fc76a
AyushSR7558 <ayushraut7558@gmail.com>
2025-08-10 19:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 152.7 MB (69.84%) - LeetSync

---
+++ 3950-maximum-k-to-sort-a-permutation/maximum-k-to-sort-a-permutation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sortPermutation(vector<int>& nums) {
+        int k = - 1;
+        for(int i = 0; i < nums.size(); i++) {
+            if(k == -1 && nums[i] != i) {
+                k = nums[i];
+            }
+            if(nums[i] != i) {
+                k &= nums[i];
+            }
+        }
+        return (k == -1? 0: k);
+    }
+};
