f7f0eb27074d670f334f913085da9e7b9425b970
AyushSR7558 <ayushraut7558@gmail.com>
2025-08-23 09:17:26 +0530

    Time: 78 ms (42.62%) | Memory: 13.9 MB (42.54%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,26 +1,17 @@
 class Solution {
 private:
-    int Minimize(int n, int indx,vector<vector<int>>&dp) {
-        // Bases cases
+    int helper(int n, vector<int>& dp) {
         if(n == 0) return 0;
-        if(indx * indx > n) return 1e4;
-        if(dp[n][indx] != -1) return dp[n][indx];
-
-
-        // First Case
-        int notTake = 1e4, Take = 1e4;
-
-        notTake = Minimize(n , indx + 1, dp);
-
-        if(n >= indx * indx) {
-            Take = 1 + Minimize(n - indx * indx, indx, dp);
+        if(dp[n]!= -1) return dp[n];
+        int mini = n;
+        for(int i = 1; i * i <= n; i++) {
+            mini = min(1 + helper(n - i * i,dp), mini);
         }
-        return dp[n][indx] = min(notTake,Take);
+        return dp[n] = mini;
     }
 public:
     int numSquares(int n) {
-        int x = sqrt(n);
-        vector<vector<int>>dp(n + 1, vector<int>(x + 1, -1));
-        return Minimize(n,1,dp);
+        vector<int> dp(n + 1, -1);
+        return helper(n,dp);
     }
 };
