ac1bf35c886092651e067256e21e9732e675258f
AyushSR7558 <ayushraut7558@gmail.com>
2025-08-25 10:09:51 +0530

    Time: 5 ms (53.37%) | Memory: 27.8 MB (31.80%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -3,21 +3,28 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
         vector<int> nums;
-        map<int,vector<int>> mp;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                mp[i + j].push_back(mat[i][j]);
+        nums.reserve(m * n); // \U0001f680 avoid reallocations
+
+        // Use vector instead of map (since keys are 0..m+n-2)
+        vector<vector<int>> diag(m + n - 1);
+
+        // Fill diagonals
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                diag[i + j].push_back(mat[i][j]);
             }
         }
-        for(int i = 0; i <= m + n - 2; i++) {
-            vector<int> init = mp[i];
-            if(!(i & 1)) {
-                reverse(init.begin(),init.end());
-            }
-            for(int i = 0; i < init.size(); i++) {
-                nums.push_back(init[i]);
+
+        // Traverse diagonals
+        for (int d = 0; d < m + n - 1; d++) {
+            if (d % 2 == 0) {
+                // reverse order push
+                nums.insert(nums.end(), diag[d].rbegin(), diag[d].rend());
+            } else {
+                nums.insert(nums.end(), diag[d].begin(), diag[d].end());
             }
         }
+
         return nums;
     }
 };
