add0e9401e7e149c9981d89def697b031afbf2e2
AyushSR7558 <ayushraut7558@gmail.com>
2025-09-17 11:45:56 +0530

    Time: 161 ms (5.43%) | Memory: 9.5 MB (95.53%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,17 +1,19 @@
 class Solution {
 private:
-    int helper(int i, int j, vector<int>& nums) {
-        if(i  > j) return 0;
+    int minimax(int i, int j, int turn, vector<int>& nums) {
+    if(i > j) return 0;
 
-        if(i == j) return nums[i];
-
-        int leftDiff = nums[i] - helper(i + 1, j, nums);
-        int rightDiff = nums[j] - helper(i,j - 1, nums);
-
-        return max(leftDiff, rightDiff);
+    if(turn % 2 == 0) { // Player 1's turn (maximize)
+        return max(nums[i] + minimax(i+1, j, turn+1, nums),
+                   nums[j] + minimax(i, j-1, turn+1, nums));
+    } else { // Player 2's turn (minimize Player 1's final score)
+        return min(-nums[i] + minimax(i+1, j, turn+1, nums),
+                   -nums[j] + minimax(i, j-1, turn+1, nums));
     }
+}
 public:
     bool predictTheWinner(vector<int>& nums) {
-        return helper(0, nums.size() - 1, nums) >= 0;
+        int val = minimax(0, nums.size() - 1, 0, nums);
+        return val >= 0;
     }
 };
