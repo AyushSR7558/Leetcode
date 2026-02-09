7d5a9ce356d00c4c6bfd7ae05a543fcc314e5375
AyushSR7558 <ayushraut7558@gmail.com>
2026-02-09 10:45:50 +0530

    Time: 7 ms (73.94%) | Memory: 43.9 MB (65.85%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,43 +1,70 @@
-class Solution {
-private:
-    int dfs(vector<vector<int>>& adjList, vector<bool>& visited, int u) {
-        int countEdges = 0, edges = 0;
-        visited[u] = true;
-        for(int v : adjList[u]) {
-            countEdges++;
-            if(!visited[v]) {
-                countEdges += dfs(adjList, visited, v);
-                edges+= 2;
-            }
-        }
+class Disjoint{
+    vector<int> rank;
+    vector<int> parent;
+    public:
 
-        return countEdges - edges;
+    Disjoint(int n) {
+        rank.resize(n, 0);
+        parent.resize(n);
+
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
     }
 
+    int findUParent(int u) {
+        if(u == parent[u]) {
+            return u;
+        }
+        return parent[u] = findUParent(parent[u]);
+    }
+
+    bool unionByRank(int u, int v) {
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+        if(ulp_u == ulp_v) {
+            return true;
+        }
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+        return false;
+    }
+
+    int totComponent() {
+        for(int i = 0; i < parent.size(); i++) {
+            findUParent(i);
+        }
+        unordered_set<int> st;
+        for(int i = 0; i < parent.size(); i++) {
+            st.insert(parent[i]);
+        }
+        return st.size();
+    }
+};
+
+class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-         vector<bool> visited(n, false);
-        vector<vector<int>> adjList(n);
-        int countComponent = 0, extraEdges = 0;
+        int extraedges = 0;
+        Disjoint a(n);
 
         for(int i = 0; i < connections.size(); i++) {
             int u = connections[i][0], v = connections[i][1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
+            if(a.unionByRank(u, v)) extraedges++;
         }
-        
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                extraEdges += dfs(adjList, visited, i) / 2;
-                countComponent++;
-            }
+        int tot = a.totComponent();
+
+        if(extraedges >= tot - 1) {
+            return tot - 1;
         }
-
-        if(extraEdges >= countComponent - 1) {
-            return countComponent - 1;
-        }
-
-
         return -1;
     }
 };
