51c5d31e8396955cbcd85b52c5ac3301130c84ee
Ayush7558 <ayushraut7558@gmail.com>
2026-02-15 23:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (80.99%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,53 +1,21 @@
 class Solution {
 public:
-    string add(string a, string b) {
-        int count = 0, carry = 0;
-        int i1 = a.length() - 1, i2 = b.length() - 1;
-        string ans = "";
-        while (i1 >= 0 && i2 >= 0) {
-            int x = a[i1] - '0', y = b[i2] - '0';
-            int count = x + y + carry;
-            if (count == 3) {
-                ans = '1' + ans;
-                carry = 1;
-            } else if (count == 2) {
-                ans = '0' + ans;
-                carry = 1;
-            } else if (count == 1) {
-                ans = '1' + ans;
-                carry = 0;
-            } else {
-                ans = '0' + ans;
-                carry = 0;
-            }
-            i1--;
-            i2--;
-        }
-        while (i1 >= 0) {
-            int x = a[i1] - '0';
-            count = x + carry;
-            if (count == 2) {
-                ans = '0' + ans;
-                carry = 1;
-            } else if (count == 1) {
-                ans = '1' + ans;
-                carry = 0;
-            } else {
-                ans = '0' + ans;
-                carry = 0;
-            }
-            i1--;
-        }
-        if(carry) ans = '1'+ans;
-        return ans;
-    }
     string addBinary(string a, string b) {
-        int i1 = a.length() - 1, i2 = b.length() - 1;
-        string ans = "";
-        if (i1 > i2) {
-            ans = add(a, b);
-        } else
-            ans = add(b, a);
-        return ans;
+        int i = a.size() - 1, j = b.size() - 1;
+        int carry = 0;
+        string res = "";
+
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if(i >= 0) sum += a[i--] - '0';
+            if(j >= 0) sum += b[j--] - '0';
+
+            res.push_back((sum % 2) + '0'); // current bit
+            carry = sum / 2;
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
