017e1fd9508e831e1c9773108364419c9a5bce84
AyushSR7558 <ayushraut7558@gmail.com>
2026-02-19 09:27:19 +0530

    Time: 7 ms (30.82%) | Memory: 16.7 MB (21.83%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     int countBinarySubstrings(string s) {
         int n = s.size();
-        int p = 0, q = 0, cnt = 0;
-        
+        vector<int> group;
+        int cnt = 0;
+
         for(int i = 0; i < n; i++) {
-            if(s[i] == '0') {
-                if(i > 0 && s[i] != s[i - 1]) p = 0;
-                p++;
-                if(q >= p) cnt++;
-            }else {
-                if(i > 0 && s[i] != s[i - 1]) q = 0;
-                q++;
-                if(p >= q) cnt++;
+            if(i == 0 || s[i] == s[i - 1]) {
+                cnt++;
+            } else {
+                group.push_back(cnt);
+                cnt = 1;
             }
         }
+        group.push_back(cnt);
+        
+        cnt = 0;
+        for(int i = 0; i < group.size() - 1; i++) {
+            cnt += min(group[i], group[i + 1]);
+        }
         return cnt;
+        
     }
 };
