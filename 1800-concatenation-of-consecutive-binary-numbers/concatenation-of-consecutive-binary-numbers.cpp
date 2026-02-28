03142c99fffd409c30aeb26997cd284908e6f5e3
AyushSR7558 <ayushraut7558@gmail.com>
2026-02-28 09:52:37 +0530

    Time: 24 ms (82.55%) | Memory: 8 MB (69.80%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -1,33 +1,14 @@
 class Solution {
-private:
-    string toBinary(int n) {
-        string s; 
-
-        while(n) {
-            s += (n & 1) + '0';
-            n = n >> 1;
-        }
-        reverse(s.begin(), s.end());
-        return s;
-    }
 public:
     int concatenatedBinary(int n) {
-        string s;
-        long long mod = 1e9 + 7;
+        const int MOD = 1e9 + 7;
+        long long res = 0;
+        int bits = 0;
 
-        for(int i = 0; i <= n; i++) {
-            s += toBinary(i);
+        for(int i=1; i<=n; i++){
+            if((i & (i-1)) == 0) bits++;
+            res = ((res<<bits) | i) % MOD;
         }
-
-        long long pow = 1;
-        long long cnt = 0;
-        for(int i =s.size() - 1; i >= 0; i--) {
-            if(s[i] == '1') {
-                cnt = (cnt + pow) % mod;
-            }
-            pow = (pow << 1);
-            pow = (pow % mod);
-        }
-        return cnt;
+        return res;
     }
 };
