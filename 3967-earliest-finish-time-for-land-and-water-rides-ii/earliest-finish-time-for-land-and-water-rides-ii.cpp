42f371dad2bbf329d947798ffac14e0ade846308
AyushSR7558 <ayushraut7558@gmail.com>
2026-06-03 08:14:59 +0530

    Time: 4 ms (73.39%) | Memory: 238.1 MB (66.06%) - LeetSync

---
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+         return min(
+            findEarliestFinish(landStartTime,  landDuration,
+                               waterStartTime, waterDuration),
+            findEarliestFinish(waterStartTime, waterDuration,
+                               landStartTime,  landDuration)
+        );
+        
+    }
+        int findEarliestFinish(vector<int>& firstStartTime,
+                           vector<int>& firstDuration,
+                           vector<int>& secondStartTime,
+                           vector<int>& secondDuration) {
+        int n = firstStartTime.size(), m = secondStartTime.size();
+
+        // Find the earliest time we can finish any first-type ride
+        int earliestFirstEnd = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            earliestFirstEnd = min(earliestFirstEnd,
+                                   firstStartTime[i] + firstDuration[i]);
+        }
+
+        // Now find the earliest finish for a second-type ride taken after
+        int result = INT_MAX;
+        for (int i = 0; i < m; i++) {
+            int waitUntil = max(earliestFirstEnd, secondStartTime[i]);
+            result = min(result, waitUntil + secondDuration[i]);
+        }
+
+        return result;
+    }
+};
