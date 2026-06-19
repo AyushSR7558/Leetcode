fa9b588665db5d1f4a80ec39ca715e467a6792fc
AyushSR7558 <ayushraut7558@gmail.com>
2026-06-19 20:43:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (94.12%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxAltitude = 0, altitude = 0;
+
+        for(int i = 0; i < gain.size(); i++) {
+            altitude += gain[i];
+            maxAltitude = max(maxAltitude, altitude);
+        }
+
+        return maxAltitude;
+    }
+};
