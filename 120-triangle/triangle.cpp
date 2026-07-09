05cdbe52aca3272e6368e1c6515b4bcbecafedd0
AyushSR7558 <ayushraut7558@gmail.com>
2026-07-09 10:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (8.80%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,15 +1,29 @@
 class Solution {
+private:
+    int helper(vector<vector<int>>& triangle, int i, int j, vector<vector<int>>& dp) {
+        // If we are ceil {i, j} what is the min value that we can get if we start from that ceil
+        int m = triangle.size(), n = i + 1; 
+
+        // Base case
+        if(i == m - 1) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j] = triangle[i][j] + min(helper(triangle, i + 1, j, dp), helper(triangle, i + 1, j + 1, dp));
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<int> dp = triangle[n - 1]; // start with last row
+        int m = triangle.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
 
-        // move from second-last row to top
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+        for(int i = 0; i < m; i++) {
+            dp[m - 1][i] = triangle[m - 1][i];
+        }
+        
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j = 0; j < i + 1; j++) {
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
             }
         }
-        return dp[0]; // top element
+        return dp[0][0];
     }
 };
