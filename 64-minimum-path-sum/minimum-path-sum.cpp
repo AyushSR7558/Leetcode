f04e308587cae557e2f50b3cc15d3a9085e3b791
AyushSR7558 <ayushraut7558@gmail.com>
2026-07-09 09:37:37 +0530

    Time: 3 ms (30.06%) | Memory: 16.2 MB (61.51%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,13 +1,5 @@
 class Solution {
-private:
-    int helper(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp) {
-        // The function will return maximum we can get if we are at the ceil {i, j}
-        if(i < 0 || j < 0) return 1e9 + 7;
-        if(i == 0 && j == 0) return grid[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[i][j] = min(grid[i][j] + helper(grid, i - 1, j, dp), grid[i][j] + helper(grid, i, j - 1, dp));
-    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
@@ -26,12 +18,7 @@ public:
 
             }
         }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                cout << dp[i][j] << " ";
-            }
-            cout << endl;
-        }
+
         return dp[m - 1][n - 1];
         
     }
