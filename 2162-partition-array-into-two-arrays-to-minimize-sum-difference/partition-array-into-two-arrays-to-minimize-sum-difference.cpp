ed74fa778bb01cd59bd00bc14b36406fd68ab2fd
AyushSR7558 <ayushraut7558@gmail.com>
2026-07-10 11:36:46 +0530

    Time: 416 ms (40.53%) | Memory: 73.4 MB (76.02%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,46 +1,46 @@
+/*
+Here's a solution that has the same underlying concept, but builds on it with some modifications to make the calculation a little easier. The idea is that instead of storing raw sums, store the net sums (i.e. if the first half has 8 numbers and we've calculated the sum of three of them, subtract the sum of the other 5). So say an array has 16 elements, the left side ("a") has the net sum of 3 elements, then we're looking for the right side ("b") with the net sum of 5 elements. In that case, (a + b) represents the net sum of a total of 8 elements and we want this to be as close to 0 as possible. So, a + b = 0, or b = -a. Hence for a given 'size', for each element 'a' on the LHS, we find the number closest to '-a' in RHS for size (m - size). Once we've found it, we find the minimum value of a + b.
+*/
+
 class Solution {
 public:
     int minimumDifference(vector<int>& nums) {
-        // We have divide the array into to equal parts
-        int n = nums.size(), N = n / 2, res = 0, sum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> left(N + 1), right(N + 1);
-
-        for(int mask = 0; mask < (1 << N); mask++) {
-            int sz = 0, l = 0, r = 0;
-            for(int i = 0; i < N; i++) {
-                if(mask & (1 << i)) {
-                    sz++;
-                    l += nums[i];
-                    r += nums[i + N];
+        int i, j, n = nums.size();
+        int m = n / 2;
+        vector<vector<int>> left(m + 1), right(m + 1);
+        for (i = 0; i < (1 << m); i++) {
+            int sum_left = 0;
+            int sum_right = 0;
+            int size = 0;
+            for (j = 0; j < m; j++) {
+                if (i & (1 << j)) {
+                    size++;
+                    sum_left += nums[j];
+                    sum_right += nums[j + m];
+                }
+                else {
+                    sum_left -= nums[j];
+                    sum_right -= nums[j + m];
                 }
             }
-            left[sz].push_back(l);
-            right[sz].push_back(r);
+            left[size].push_back(sum_left);
+            right[size].push_back(sum_right);
         }
-
-        for(int sz = 0; sz <= N; ++sz) {
-            sort(right[sz].begin(), right[sz].end());
-        }
-
-        res = min(abs(sum - 2 *left[N][0]), abs(sum - 2 * right[N][0]));
-
-        // iterating over left part
-        for(int sz = 1; sz < N; sz++) {
-            for(auto &a : left[sz]) {
-                int b = (sum - 2 * a) / 2, rsz = N -sz;
-                auto &v = right[rsz];
-                auto itr = lower_bound(v.begin(), v.end(), b);
-
-                if(itr != v.end()) res = min(res, abs(sum - 2 * (a + (*itr))));
-                if(itr != v.begin()) {
-                    auto it = itr; --it;
-                    res = min(res,abs(sum - 2 * (a + (*it))));
+        for (auto &arr : right)
+            sort(arr.begin(), arr.end());
+        int ret = 1e9;
+        for (int size = 0; size <= m; size++) {
+            for (auto &a : left[size]) {
+                auto &rhs = right[m - size];
+                auto it = lower_bound(rhs.begin(), rhs.end(), -a);
+                if (it != rhs.end()) {
+                    ret = min(ret, abs(a + *it));
+                }
+                if (it != rhs.begin()) {
+                    ret = min(ret, abs(a + *prev(it)));
                 }
             }
         }
-
-        return res;
-
-        
+        return ret;        
     }
 };
