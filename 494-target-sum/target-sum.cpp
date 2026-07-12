d52ac125954975d7e9ecf3beeb9b30bdef37934e
AyushSR7558 <ayushraut7558@gmail.com>
2026-07-12 07:20:02 +0530

    Time: 319 ms (25.74%) | Memory: 78.9 MB (5.01%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private:
-    int helper(vector<int>& nums, int target, int indx) {
+    int helper(vector<int>& nums, int target, int indx, unordered_map<string, int>& dp) {
+        string key = to_string(target) + "_" + to_string(indx);
         if(indx == 0) {
             if(target + nums[indx] == 0 && target - nums[indx] == 0) {
                 return 2;
@@ -11,12 +12,17 @@ private:
             return 0;
         }
 
+        if(dp.contains(key)) {
+            return dp[key];
+        }
+
         // Either we can add or sub
-        return helper(nums, target + nums[indx], indx - 1) + helper(nums, target - nums[indx], indx - 1);
+        return dp[key] = helper(nums, target + nums[indx], indx - 1, dp) + helper(nums, target - nums[indx], indx - 1, dp);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        return helper(nums, target, n - 1);
+        unordered_map<string, int> dp;
+        return helper(nums, target, n - 1, dp);
     }
 };
