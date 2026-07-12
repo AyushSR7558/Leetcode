121b4d84f0703880594e64c784a28234f48c33e6
AyushSR7558 <ayushraut7558@gmail.com>
2026-07-12 07:12:46 +0530

    Time: 431 ms (20.97%) | Memory: 11.6 MB (95.01%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 private:
     int helper(vector<int>& nums, int target, int indx) {
-        // Base case 
         if(indx == 0) {
-            if(nums[0] == 0 && target == 0) return 2;
-            if(target + nums[indx] == 0 || target - nums[indx] == 0) return 1;
+            if(target + nums[indx] == 0 && target - nums[indx] == 0) {
+                return 2;
+            }
+            if(target + nums[indx] == 0 || target - nums[indx] == 0) {
+                return 1;
+            }
             return 0;
         }
 
-        int count = helper(nums, target -nums[indx], indx -1) + helper(nums, target + nums[indx], indx - 1);
-
-        return count;
+        // Either we can add or sub
+        return helper(nums, target + nums[indx], indx - 1) + helper(nums, target - nums[indx], indx - 1);
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size() - 1;
-        return helper(nums, target, n);
+        int n = nums.size();
+        return helper(nums, target, n - 1);
     }
 };
